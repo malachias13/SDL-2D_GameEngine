a28270d2ee94375751b4692e5d74d8e857fdc892
Malachias Harris <69220988+malachias13@users.noreply.github.com>
2024-05-16 02:21:41 -0400

    Added a sprite and move comp to class.

--- SDL-2D_GameEngine/Game/Stars.cpp
+++ SDL-2D_GameEngine/Game/Stars.cpp
@@ -1,9 +1,22 @@
 #include "Stars.h"
 #include "../GameMath.h"
+#include "../Random.h"
+#include "../Components/SpriteComponent.h"
+#include "../Components/MoveComponent.h"
+#include "../Game.h"
 
 Stars::Stars(Game* game)
 	:Actor(game)
 {
 	// Initialize to random position.
-	//Vector randPos = Random::Get
+	Vector2 randPos = Random::GetVector(Vector2::Zero, Vector2(1024.0f, 768.0f));
+	SetPosition(randPos);
+	SetRotation(Random::GetFloatRange(0.0f, Math::TwoPi));
+
+	// Create a sprite comp, and set texture
+	SpriteComponent* sc = new SpriteComponent(this);
+	sc->SetTexture(game->GetTexture("Assets/Purple Star Icon 2.png"));
+
+	// Create a move component, and set a forward speed;
+	MoveComponent* mc = new MoveComponent(this);
 }
