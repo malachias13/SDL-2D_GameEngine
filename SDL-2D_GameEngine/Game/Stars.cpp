3f1cc4d6db111fea1b4046eedb5e91ad596511f8
Malachias Harris <69220988+malachias13@users.noreply.github.com>
2024-05-16 02:03:31 -0400

    Added star class

---
+++ SDL-2D_GameEngine/Game/Stars.cpp
@@ -0,0 +1,9 @@
+#include "Stars.h"
+#include "../GameMath.h"
+
+Stars::Stars(Game* game)
+	:Actor(game)
+{
+	// Initialize to random position.
+	//Vector randPos = Random::Get
+}
---
+++ SDL-2D_GameEngine/Game/Stars.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Actor.h"
+
+class Stars : public Actor
+{
+public:
+	Stars(class Game* game);
+};
+
